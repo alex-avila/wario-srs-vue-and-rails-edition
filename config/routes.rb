2c1d1714db8499cd5ca9093ee809dda925b060d3
Alex Avila <alex.avilx@icloud.com>
2019-09-08 14:53:25 -0600

    Set up routes
    * Set up API for decks & cards with prefix (namespaces) api/v1
      & resources helper
    * Let index action on homepage controller handle all requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :decks do
+        resources :cards
+      end
+    end
+  end
+
   root 'homepage#index'
+
+  get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
