ca8b484127af534f8e230709e6757d393b189295
Alex Avila <alex.avilx@icloud.com>
2019-09-08 15:02:27 -0600

    Create "dashboard" code to display decks from API

---
+++ app/javascript/scenes/Home/Deck.vue
@@ -0,0 +1,40 @@
+<template>
+  <a :href="deck.id" class="deck-item">
+    <div class="deck-item__wrapper">
+      <div :id="deck.id">
+        <h3>{{ deck.name }}</h3>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    deck: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.deck-item {
+  display: block;
+  border: 1px solid #d8d8d8;
+  background: #fff;
+  color: inherit;
+  text-decoration: none;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.13);
+  border-radius: 3px 3px 0 0;
+}
+
+.deck-item__wrapper {
+  text-align: center;
+}
+
+.deck-item__wrapper h3 {
+  font-weight: 500;
+}
+</style>
---
+++ app/javascript/scenes/Home/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="decks-list">
+    <h2 class="utility-wrapper">Decks</h2>
+    <div class="decks-list__wrapper utility-wrapper">
+      <Deck v-for="deck in decks" :key="deck.id" :deck="deck" />
+    </div>
+  </section>
+</template>
+
+<script>
+import Deck from "./Deck";
+
+export default {
+  components: { Deck },
+
+  computed: {
+    decks() {
+      return this.$store.state.decks;
+    }
+  },
+
+  mounted() {
+    this.$store.dispatch("getDecks");
+  }
+};
+</script>
+
+<style>
+.decks-list {
+  padding-bottom: 2em;
+}
+
+.decks-list h2 {
+  margin: 0.5em auto;
+}
+
+.decks-list__wrapper {
+  display: grid;
+  grid-gap: 1em;
+}
+
+@media (min-width: 600px) {
+  .decks-list__wrapper {
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  }
+}
+</style>
