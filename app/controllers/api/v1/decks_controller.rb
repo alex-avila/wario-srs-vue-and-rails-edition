bfa9f3ed9beb1e7141231dd32e6853600f54994c
Alex Avila <alex.avilx@icloud.com>
2019-09-08 14:56:32 -0600

    Generate "Decks" controller & write index, show, & create actions

---
+++ app/controllers/api/v1/decks_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::DecksController < ApplicationController
+  def index
+    decks = Deck.all
+
+    render json: decks
+  end
+
+  def show
+    deck = Deck.find(params[:id])
+
+    render json: deck
+  end
+
+  def create
+    deck = Deck.new(deck_params)
+
+    if deck.save
+      render json: deck
+    else
+      render json: deck.errors
+    end
+  end
+
+  private
+    def deck_params
+      params.require(:deck).permit(:name, :description)
+    end
+end
