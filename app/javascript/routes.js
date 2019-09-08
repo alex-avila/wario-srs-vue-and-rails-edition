34528159f352f241a33d7f49b3835417e838747c
Alex Avila <alex.avilx@icloud.com>
2019-09-08 16:08:53 -0600

    Add new route for viewing deck details

--- app/javascript/routes.js
+++ app/javascript/routes.js
@@ -1,6 +1,8 @@
 import Home from './scenes/Home'
+import DeckDetails from './scenes/DeckDetails'
 
 export default [
   { path: '/', redirect: '/dashboard' },
-  { path: '/dashboard', component: Home }
+  { path: '/dashboard', component: Home },
+  { path: '/decks/:id', component: DeckDetails }
 ]
