a600c3d62c1277683b3cf7ff4db80ad12cce7894
Alex Avila <alex.avilx@icloud.com>
2019-09-08 21:45:53 -0600

    Add actions to show all and create cards

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::CardsController < ApplicationController
+  def index
+    cards = Card.all
+
+    render json: cards
+  end
+
+  def create
+    @deck = Deck.find(params[:deck_id])
+    card = @deck.cards.create(card_params)
+
+    render json: card
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:front, :back, :deck)
+    end
 end
