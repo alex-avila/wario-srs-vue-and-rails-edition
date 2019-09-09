ccae6811f093b363909f7021e7cb91d06a6a6f16
Alex Avila <alex.avilx@icloud.com>
2019-09-08 21:51:31 -0600

    Add "/decks/:id/review-session" route

--- app/javascript/routes.js
+++ app/javascript/routes.js
@@ -1,8 +1,10 @@
 import Home from './scenes/Home'
 import DeckDetails from './scenes/DeckDetails'
+import ReviewSession from './scenes/ReviewSession'
 
 export default [
   { path: '/', redirect: '/dashboard' },
   { path: '/dashboard', component: Home },
-  { path: '/decks/:id', component: DeckDetails }
+  { path: '/decks/:id', component: DeckDetails },
+  { path: '/decks/:id/review-session', component: ReviewSession }
 ]
