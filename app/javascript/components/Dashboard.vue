4cece6eb050eb5a8e2d80e1bf75028b884291acc
Alex Avila <alex.avilx@icloud.com>
2019-09-09 00:54:50 -0600

    Finish Dashboard

--- app/javascript/components/Dashboard.vue
+++ app/javascript/components/Dashboard.vue
@@ -2,18 +2,18 @@
   <div class="dashboard">
     <h3>Dashboard</h3>
     <div class="dashboard__wrapper">
-      <!-- <div>
+      <div>
         <span>{{ nextReviewDate }}</span>
         <p>Next Review</p>
-      </div> -->
+      </div>
       <div>
         <span>{{ cardsAvailableNow.length }}</span>
         <p>Available Now</p>
       </div>
-      <!-- <div>
-        <span>{{ cardsAvailableTomorrow }}</span>
+      <div>
+        <span>{{ cardsAvailableTomorrowNum }}</span>
         <p>Next Day</p>
-      </div> -->
+      </div>
     </div>
     <h4>
       Percentage of cards seen <small>( {{ percentageText }}% )</small>
@@ -32,7 +32,11 @@ export default {
   computed: {
     ...mapState("cards", ["cards"]),
 
-    ...mapGetters("cards", ["nextReviewDate", "cardsAvailableNow"]),
+    ...mapGetters("cards", [
+      "nextReviewDate",
+      "cardsAvailableNow",
+      "cardsAvailableTomorrowNum"
+    ]),
 
     percentage() {
       if (this.cards.length) {
@@ -51,64 +55,6 @@ export default {
     }
   }
 };
-
-// class Dashboard extends Component {
-//     render() {
-//         const { cards } = this.props
-//         const { availableNow, nextDay, nextReview } = this.props.dashboardData
-//         let percentage = 0
-//         let momentDate = 'never'
-//         const areReviewsReady = new Date(nextReview).getUTCDate() === new Date(Date.now()).getUTCDate()
-//         // reviews are ready but there are none available
-//         if (areReviewsReady && availableNow === 0) {
-//             momentDate = 'in a day'
-//         } else if (areReviewsReady) {
-//             momentDate = 'available now'
-//         } else {
-//             if (cards.length) {
-//                 momentDate = moment(nextReview).fromNow()
-//             } else {
-//                 momentDate = 'never'
-//             }
-//         }
-//         const seen = this.props.cards.reduce((final, card) => {
-//             return card.hasBeenSeen ? final + 1 : final
-//         }, 0)
-//         percentage = (seen / this.props.cards.length) * 100
-//         return (
-//             <div className="dashboard">
-//                 <h3>Dashboard</h3>
-//                 <div className="dashboard__wrapper">
-//                     <div>
-//                         <span>
-//                             {
-//                                 new Date(nextReview) > Date.now() || !cards.length || new Date(nextReview).getUTCDate() === new Date(Date.now()).getUTCDate() ?
-//                                     momentDate.slice(0, 1).toUpperCase() + momentDate.slice(1) :
-//                                     'Available Now'
-//                             }
-//                         </span>
-//                         <p>Next Review</p>
-//                     </div>
-//                     <div>
-//                         <span>{availableNow ? availableNow : 0}</span>
-//                         <p>Available Now</p>
-//                     </div>
-//                     <div>
-//                         <span>{nextDay ? nextDay : 0}</span>
-//                         <p>Next Day</p>
-//                     </div>
-//                 </div>
-//                 <h4>
-//                     Percentage of cards studied <small>({percentage !== 0 && percentage < 1 ?
-//                         '<1' : Math.round(percentage)}%)</small>
-//                 </h4>
-//                 <ProgressBar percentage={percentage} />
-//             </div>
-//         );
-//     }
-// }
-
-// export default Dashboard;
 </script>
 
 <style>
