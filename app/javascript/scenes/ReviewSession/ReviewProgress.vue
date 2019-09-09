5f19c7a187aacfd8c6e50cf23fd39b909b2f0a2f
Alex Avila <alex.avilx@icloud.com>
2019-09-08 21:50:47 -0600

    Write ReviewProgress component

---
+++ app/javascript/scenes/ReviewSession/ReviewProgress.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="review-progress-bar">
+    <div class="progress-bar__wrapper">
+      <div
+        :style="{ width: `${percentage}%`, height: 10 }"
+        class="progress-bar__progression"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    percentage: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+@media (min-width: 600px) {
+  .review-progress-bar .progress-bar__wrapper {
+    position: absolute;
+    top: 0;
+    z-index: 3;
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid #d8d8d8;
+  }
+}
+</style>
